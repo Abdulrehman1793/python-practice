afa90007355c31b5481c00461832950f7c28ae14
Khan Khalid <khankhalid2054@gmail.com>
2025-04-15 15:45:04 +0530

    Implemented Task #1 of issue #1

---
+++ pancard-validator.py
@@ -0,0 +1,12 @@
+panCard='ABCP04563O';
+
+# Task 1: Accepts a string representing a 10-digit PAN number as an argument.
+
+if(len(panCard)>10):
+    print("Invalid pan card");
+    exit();
+    
+# Task 2:
+
+    
+    
