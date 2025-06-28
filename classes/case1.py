aecef6eafe1a781f451527a7013a73b15e2f9e8d
Khan Khalid <khankhalid2054@gmail.com>
2025-06-28 12:22:05 +0530

    Completed issues #3

--- classes/case1.py
+++ classes/case1.py
@@ -6,9 +6,11 @@ class Animal:
 
     def make_sound(self):
          print(f'{self.name.title()} makes a sound')
-     
+
+# Example 1
 catAnimal = Animal('cat','domestic_cat')
 catAnimal.make_sound()
 
+# Example 2
 dogAnimal = Animal('dog', 'bull_dog')
 dogAnimal.make_sound()
