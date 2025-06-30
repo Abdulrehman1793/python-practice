906d90945257f171c0b85af1669e881a9a566a63
Khan Khalid <khankhalid2054@gmail.com>
2025-06-30 12:39:14 +0530

    completed case study 3 : CarSimulation

---
+++ classes/case 3.py
@@ -0,0 +1,38 @@
+#case :- Simple Car Simulation 
+class CarSimulation :
+    def __init__(self, make, models, years):
+         self.make = make
+         self.models = models
+         self.years = years
+         self.is_engine_on = False
+
+    def start_engine(self):
+         if self.is_engine_on==False:
+            self.is_engine_on = True
+         else:
+            print("Engkine is aLREADY ON...")
+
+    def stop_engine(self):
+         if self.is_engine_on == True:
+            self.is_engine_on = False
+         else:
+             print("engine is off")
+    
+    def drive(self):
+        if self.is_engine_on == True:
+            print(f'{self.make}, {self.models},"is driving')
+        else:
+            print("engine is off  Please start the engine" )
+    
+#example
+
+car = CarSimulation ("ford","mustang","1999" )
+car.stop_engine()
+car.drive()
+
+
+             
+
+
+
+          
